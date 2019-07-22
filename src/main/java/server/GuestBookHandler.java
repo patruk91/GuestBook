d1bb2a44b77110176ef6120edabd2380bb0c020d
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-22 14:32:39 +0200

    Add methods to display all records from database

--- src/main/java/server/GuestBookHandler.java
+++ src/main/java/server/GuestBookHandler.java
@@ -2,21 +2,35 @@ package server;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import dao.IGuestBookDao;
+import model.GuestForm;
 
 import java.io.*;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GuestBookHandler implements HttpHandler {
+    private IGuestBookDao guestBookDao;
+
+    public GuestBookHandler(IGuestBookDao guestBookDao) {
+        this.guestBookDao = guestBookDao;
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String response = "";
         String method = exchange.getRequestMethod();
 
         if (method.equals("GET")) {
-            response = createHTMLForm();
+            response = "<html><body>\n" +
+                    displayAllRecords() +
+                    createHTMLForm() +
+                    "</body></html>";
         }
 
         if(method.equals("POST")) {
@@ -25,6 +39,11 @@ public class GuestBookHandler implements HttpHandler {
             String formData = bufferedReader.readLine();
             System.out.println(formData);
             Map<String, String> inputs = parseFromData(formData);
+            guestBookDao.insertRecord(inputs.get("name"), inputs.get("message"));
+            response = "<html><body>\n" +
+                    displayAllRecords() +
+                    createHTMLForm() +
+                    "</body></html>";
 
         }
 
@@ -33,6 +52,22 @@ public class GuestBookHandler implements HttpHandler {
         outputStream.write(response.getBytes());
         outputStream.close();
 
+    }
+
+    private String displayAllRecords() {
+        StringBuilder stringBuilder = new StringBuilder();
+        List<GuestForm> guestsForms = guestBookDao.getAllRecords();
+        guestsForms.forEach(record -> stringBuilder.append(createSingleRecord(record)));
+        return stringBuilder.toString();
+    }
+
+    private String createSingleRecord(GuestForm record) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        return String.format("<div class=\"input\">\n" +
+                "    <div class=\"message\">%s</div>\n" +
+                "    <div class=\"name\">%s</div>\n" +
+                "    <div class=\"date\">%s</div>\n" +
+                "</div>", record.getMessage(), record.getName(), formatter.format(record.getDate()));
     }
 
     private Map<String, String> parseFromData(String formData) throws UnsupportedEncodingException {
@@ -47,14 +82,12 @@ public class GuestBookHandler implements HttpHandler {
     }
 
     private String createHTMLForm() {
-        return "<html><body>\n" +
-                "    <form method=\"POST\">\n" +
+        return  "    <form method=\"POST\">\n" +
                 "    <label for=\"name\">Name</label><br>\n" +
                 "    <input id=\"name\" type=\"text\" name=\"name\" placeholder=\"Name\"><br><br>\n" +
                 "    <label for=\"message\">Message</label><br>\n" +
                 "    <textarea name=\"message\" id=\"message\" placeholder=\"Message...\"></textarea><br><br>\n" +
                 "    <input type=\"submit\" value=\"Submit\">\n" +
-                "    </form>\n" +
-                "</body></html>";
+                "    </form>\n";
     }
 }
