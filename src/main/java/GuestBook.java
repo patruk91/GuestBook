e9276441ee74dac63b8428ac5ecd939ee86aa015
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-22 11:06:08 +0200

    Implement connection pool to database

--- src/main/java/GuestBook.java
+++ src/main/java/GuestBook.java
@@ -1,5 +1,18 @@
+import dao.sql.ConnectionPool;
+
+import java.sql.SQLException;
+
 public class GuestBook {
     public static void main(String[] args) {
+        final String URL = "jdbc:postgresql://192.168.10.171:5432/guestbook";
+        final String USER = "pl";
+        final String PASSWORD = "postgres";
 
+        ConnectionPool connectionPool = null;
+        try {
+            connectionPool = ConnectionPool.create(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
