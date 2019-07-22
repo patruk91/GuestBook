8ff10e2c580500afb6e2482d213cedd722d5b730
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-22 14:27:26 +0200

    creation connection pool from controller

--- src/main/java/GuestBook.java
+++ src/main/java/GuestBook.java
@@ -1,5 +1,10 @@
 import controller.Controller;
+import dao.IGuestBookDao;
+import dao.sql.ConnectionPool;
+import dao.sql.GuestBookSQL;
 import server.ServerGuestBook;
+
+import java.sql.SQLException;
 
 
 public class GuestBook {
@@ -7,9 +12,15 @@ public class GuestBook {
         final String URL = "jdbc:postgresql://localhost:5432/guestbook";
         final String USER = "pl";
         final String PASSWORD = "postgres";
-        ServerGuestBook serverGuestBook = new ServerGuestBook();
 
-
+        ConnectionPool connectionPool = null;
+        try {
+            connectionPool = ConnectionPool.create(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        IGuestBookDao guestBookDao = new GuestBookSQL(connectionPool);
+        ServerGuestBook serverGuestBook = new ServerGuestBook(guestBookDao);
         Controller controller = new Controller(URL, USER, PASSWORD, serverGuestBook);
         controller.run();
     }
