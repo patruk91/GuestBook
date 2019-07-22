3afe72d6d5933a7df56823212f86eaf5c869347d
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-07-22 14:31:31 +0200

    Add a field, constructor to connect with database

--- src/main/java/server/ServerGuestBook.java
+++ src/main/java/server/ServerGuestBook.java
@@ -1,15 +1,21 @@
 package server;
 
 import com.sun.net.httpserver.HttpServer;
+import dao.IGuestBookDao;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class ServerGuestBook {
+    private IGuestBookDao guestBookDao;
+
+    public ServerGuestBook(IGuestBookDao guestBookDao) {
+        this.guestBookDao = guestBookDao;
+    }
 
     public void startServer() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/", new GuestBookHandler());
+        server.createContext("/", new GuestBookHandler(guestBookDao));
         server.setExecutor(null);
         server.start();
 
